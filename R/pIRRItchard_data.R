ed2a58b9d99057f842e3458aa7d0ec44310a2f55
tylerpritchard <tyler.pritchard@uoguelph.ca>
2021-03-13 18:24:31 -0500

    n
    jj

---
+++ R/pIRRItchard_data.R
@@ -0,0 +1,13 @@
+piRRitchard_data <- function(criteria, raters, units){
+  dat <- data.frame(id = 1:units)
+  vars <- replicate(criteria*raters, round(runif(units)))
+  crit <- paste0("Criteria", 1:criteria)
+  rate <- paste0("Rater", 1:raters)
+  colnames(vars) <- apply(expand.grid(crit, rate), 1, paste, collapse=".")
+  library(tidyverse)
+  dat <- cbind(dat, vars)
+  dat <- dat %>%
+    mutate_all(as.factor)
+  return(dat)
+}
+
